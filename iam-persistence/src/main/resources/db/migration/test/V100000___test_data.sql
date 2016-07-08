616924304ed296b7af0cd52e8af8ba5da9415381
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2016-07-08 10:28:50 +0200

    Fix double insert of registration scopes

--- iam-persistence/src/main/resources/db/migration/test/V100000___test_data.sql
+++ iam-persistence/src/main/resources/db/migration/test/V100000___test_data.sql
@@ -10,9 +10,7 @@ INSERT INTO system_scope(scope, description, icon, restricted, default_scope, st
   ('phone', 'telephone number', 'bell', false, true, false, null),
   ('offline_access', 'offline access', 'time', false, false, false, null),
   ('scim:read','read access to SCIM user and groups', null, true, false, false, null),
-  ('scim:write','write access to SCIM user and groups', null, true, false, false, null),
-  ('registration:read','List registration requests', null, true, false, false, null),
-  ('registration:write','Update registration requests', null, true, false, false, null);
+  ('scim:write','write access to SCIM user and groups', null, true, false, false, null);
 
 INSERT INTO client_details (id, client_id, client_secret, client_name, dynamically_registered, refresh_token_validity_seconds, access_token_validity_seconds, id_token_validity_seconds, allow_introspection, token_endpoint_auth_method) VALUES
   (1, 'client', 'secret', 'Test Client', false, null, 3600, 600, true, 'SECRET_BASIC'),
