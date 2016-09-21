6db7afb63be8c101077624318e87352e147ed2ec
enricovianello <maverick17586@gmail.com>
2016-09-21 15:08:34 +0200

    Added research to requests page

--- iam-login-service/src/main/webapp/resources/iam/js/dashboard-app/controllers/registration.controller.js
+++ iam-login-service/src/main/webapp/resources/iam/js/dashboard-app/controllers/registration.controller.js
@@ -2,51 +2,97 @@
 
 angular.module('dashboardApp').controller('RequestManagementController', RequestManagementController);
 
-RequestManagementController.$inject = ['$scope', '$rootScope', '$state', '$filter', 'RegistrationRequestService', 'ModalService'];
+RequestManagementController.$inject = ['$scope', '$rootScope', '$state', '$filter', 'filterFilter', 'RegistrationRequestService', 'ModalService'];
 
-function RequestManagementController($scope, $rootScope, $state, $filter, RegistrationRequestService, ModalService){
+function RequestManagementController($scope, $rootScope, $state, $filter, filterFilter, RegistrationRequestService, ModalService){
 
-	var vm = this;
-	vm.operationResult;
-	vm.textAlert;
+	var requests = this;
+	requests.operationResult;
+	requests.textAlert;
 	
-	vm.listRequests = listRequests;
-	vm.listPending = listPending;
-	vm.approveRequest = approveRequest;
-	vm.rejectRequest = rejectRequest;
-	vm.init = init;
+	requests.listRequests = listRequests;
+	requests.listPending = listPending;
+	requests.approveRequest = approveRequest;
+	requests.rejectRequest = rejectRequest;
+	requests.init = init;
 
-	vm.list = [];
-	vm.sortType = 'creationTime';
-	vm.sortReverse = true;
-	vm.currentPage = 1;
-	vm.maxSize = 5;
-	vm.numPerPage = 10;
+	requests.list = [];
+	requests.filtered = [];
+	requests.sortType = 'creationTime';
+	requests.sortReverse = true;
+	requests.currentPage = 1;
+	requests.maxSize = 5;
+	requests.numPerPage = 10;
+
+	// search
+	requests.searchText = "";
+	requests.resetFilters = resetFilters;
+	requests.rebuildFilteredList = rebuildFilteredList;
+
+	function resetFilters() {
+		// needs to be a function or it won't trigger a $watch
+		requests.searchText = "";
+	}
+
+	function rebuildFilteredList() {
+
+		requests.filtered = filterFilter(requests.list, function(request) {
+
+			if (!requests.searchText) {
+				return true;
+			}
+
+			var query = requests.searchText.toLowerCase();
+			
+			if (request.familyname.toLowerCase().indexOf(query) != -1) {
+				return true;
+			}
+			if (request.givenname.toLowerCase().indexOf(query) != -1) {
+				return true;
+			}
+			if (request.username.toLowerCase().indexOf(query) != -1) {
+				return true;
+			}
+			if (request.email.toLowerCase().indexOf(query) != -1) {
+				return true;
+			}
+			if (request.notes.toLowerCase().indexOf(query) != -1) {
+				return true;
+			}
+			return false;
+		});
+	}
+
+	$scope.$watch('requests.searchText', function() {
+
+		requests.rebuildFilteredList();
+	});
 
 	function init(){
-		vm.listPending();
+		requests.listPending();
 	};
 
 	function listRequests(status) {
 		RegistrationRequestService.listRequests(status).then(
 			function(result) {
-				vm.list = result.data;
+				requests.list = result.data;
 			},
 			function(errResponse) {
-				vm.textAlert = errResponse.data.error_description || errResponse.data.detail;
-				vm.operationResult = 'err';
+				requests.textAlert = errResponse.data.error_description || errResponse.data.detail;
+				requests.operationResult = 'err';
 			})
 	};
 	
 	function listPending() {
 		RegistrationRequestService.listPending().then(
 			function(result) {
-				vm.list = result.data;
+				requests.list = result.data;
+				requests.rebuildFilteredList();
 				$rootScope.loggedUser.pendingRequests = result.data;
 			},
 			function(errResponse) {
-				vm.textAlert = errResponse.data.error_description || errResponse.data.detail;
-				vm.operationResult = 'err';
+				requests.textAlert = errResponse.data.error_description || errResponse.data.detail;
+				requests.operationResult = 'err';
 				$state.go("error", {
 					"error" : errResponse
 				});
@@ -56,13 +102,13 @@ function RequestManagementController($scope, $rootScope, $state, $filter, Regist
 	function approveRequest(request) {
 		RegistrationRequestService.updateRequest(request.uuid, 'APPROVED').then(
 			function() {
-				vm.textAlert = `${request.givenname} ${request.familyname} request approved successfully`;
-				vm.operationResult = 'ok';
-				vm.listPending();
+				requests.textAlert = `${request.givenname} ${request.familyname} request approved successfully`;
+				requests.operationResult = 'ok';
+				requests.listPending();
 			},
 			function(errResponse) {
-				vm.textAlert = errResponse.data.error_description || errResponse.data.detail;
-				vm.operationResult = 'err';
+				requests.textAlert = errResponse.data.error_description || errResponse.data.detail;
+				requests.operationResult = 'err';
 			})
 	};
 
@@ -79,13 +125,13 @@ function RequestManagementController($scope, $rootScope, $state, $filter, Regist
 				function (){
 					RegistrationRequestService.updateRequest(request.uuid, 'REJECTED').then(
 							function() {
-								vm.textAlert = `${request.givenname} ${request.familyname} request rejected successfully`;
-								vm.operationResult = 'ok';
-								vm.listPending();
+								requests.textAlert = `${request.givenname} ${request.familyname} request rejected successfully`;
+								requests.operationResult = 'ok';
+								requests.listPending();
 							},
 							function(errResponse) {
-								vm.textAlert = errResponse.data.error_description || errResponse.data.detail;
-								vm.operationResult = 'err';
+								requests.textAlert = errResponse.data.error_description || errResponse.data.detail;
+								requests.operationResult = 'err';
 							})
 				});
 	};
