f753e1aebc478e63b609d713e0290ea37825f924
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2016-10-28 20:06:41 +0200

    More token exchange tests
    - Inspect the exchanged token and check that provides all the expected
    claims
    - Check access to introspection and userinfo endpoints
    - Check that token exchange requires a user identity linked to an access
    token

--- iam-login-service/src/test/java/it/infn/mw/iam/test/oauth/TokenExchangeTests.java
+++ iam-login-service/src/test/java/it/infn/mw/iam/test/oauth/TokenExchangeTests.java
@@ -1,9 +1,18 @@
 package it.infn.mw.iam.test.oauth;
 
 import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
+import java.text.ParseException;
 import java.util.Arrays;
 
 import org.hamcrest.Matchers;
@@ -14,6 +23,10 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.nimbusds.jwt.JWT;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.JWTParser;
 
 import it.infn.mw.iam.IamLoginService;
 import it.infn.mw.iam.test.TestUtils;
@@ -31,7 +44,7 @@ public class TokenExchangeTests {
   private final String TOKEN_TYPE = "urn:ietf:params:oauth:token-type:jwt";
 
   @Test
-  public void testImpersonationFlow() {
+  public void testImpersonationFlowWithAudience() {
 
     String clientId = "token-exchange-subject";
     String clientSecret = "secret";
@@ -41,41 +54,192 @@ public class TokenExchangeTests {
 
     String audClientId = "tasks-app";
 
-    String accessToken = TestUtils.getAccessToken(clientId, clientSecret, "openid profile");
+    String accessToken = TestUtils.passwordTokenGetter(clientId, clientSecret)
+      .scope("openid profile")
+      .username("test")
+      .password("password")
+      .getAccessToken();
 
     // @formatter:off
+    String actorAccessToken = 
+      given()
+        .auth()
+          .preemptive()
+            .basic(actorClientId, actorClientSecret)
+        .port(iamPort)
+        .param("grant_type", GRANT_TYPE)
+        .param("audience", audClientId)
+        .param("subject_token", accessToken)
+        .param("subject_token_type", TOKEN_TYPE)
+        .param("scope", "read-tasks")
+      .when()
+        .post("/token")
+      .then()
+        .log()
+          .body(true)
+        .statusCode(HttpStatus.OK.value())
+        .body("scope", equalTo("read-tasks"))
+        .body("issued_token_type", equalTo(TOKEN_TYPE))
+        .body("token_type", equalTo("Bearer"))
+        .body("access_token", notNullValue())
+        .extract()
+          .path("access_token");
+    // @formatter:on
+
+    // Check audience is encoded in JWT access token
+    try {
+      JWT jwtAccessToken = JWTParser.parse(actorAccessToken);
+      JWTClaimsSet claims = jwtAccessToken.getJWTClaimsSet();
+
+      assertThat(claims.getAudience(), contains("tasks-app"));
+      assertThat(claims.getAudience(), hasSize(1));
+
+    } catch (ParseException e) {
+      fail(e.getMessage());
+    }
+
+    // Check audience is also returned by the introspection endpoint
+    // @formatter:off
+    // Introspect token
     given()
       .auth()
         .preemptive()
-          .basic(actorClientId, actorClientSecret)
+        .basic(actorClientId, actorClientSecret)
       .port(iamPort)
-      .param("grant_type", GRANT_TYPE)
-      .param("audience", audClientId)
-      .param("subject_token", accessToken)
-      .param("subject_token_type", TOKEN_TYPE)
-      .param("scope", "read-tasks")
-    .when()
-      .post("/token")
-    .then()
-      .log()
-        .body(true)
-      .statusCode(HttpStatus.OK.value())
-      .body("scope", equalTo("read-tasks"))
-      .body("issued_token_type", equalTo(TOKEN_TYPE))
-      .body("token_type", equalTo("Bearer"))
-      .body("access_token", notNullValue());
+      .formParam("token", actorAccessToken)
+      .when()
+        .post("/introspect")
+      .then()
+        .log().body(true)
+        .statusCode(HttpStatus.OK.value())
+        .body("aud", equalTo("tasks-app"))
+        .body("active", equalTo(true))
+        .body("scope", equalTo("read-tasks"))
+        .body("user_id", equalTo("test"))
+        .body("client_id", equalTo(actorClientId));
+    // @formatter:on
+
+    // @formatter:off
+    // get user info on token, this fails as we did not 
+    // request to openid scope
+    given()
+      .port(iamPort)
+      .authentication()
+      .preemptive().oauth2(actorAccessToken)
+      .when()
+        .post("/userinfo")
+      .then()
+        .log().body(true)
+        .statusCode(HttpStatus.FORBIDDEN.value());
+    // @formatter:on
+
+
+
+  }
+
+  @Test
+  public void testImpersonationFlowWithoutAudience() {
+
+    String clientId = "token-exchange-subject";
+    String clientSecret = "secret";
+
+    String actorClientId = "token-exchange-actor";
+    String actorClientSecret = "secret";
+
+    String accessToken = TestUtils.passwordTokenGetter(clientId, clientSecret)
+      .scope("openid profile")
+      .username("test")
+      .password("password")
+      .getAccessToken();
+
+    // @formatter:off
+    String actorAccessToken = 
+      given()
+        .auth()
+          .preemptive()
+            .basic(actorClientId, actorClientSecret)
+        .port(iamPort)
+        .param("grant_type", GRANT_TYPE)
+        .param("subject_token", accessToken)
+        .param("subject_token_type", TOKEN_TYPE)
+        .param("scope", "read-tasks openid profile")
+      .when()
+        .post("/token")
+      .then()
+        .log()
+          .body(true)
+        .statusCode(HttpStatus.OK.value())
+        .body("scope", equalTo("read-tasks openid profile"))
+        .body("issued_token_type", equalTo(TOKEN_TYPE))
+        .body("token_type", equalTo("Bearer"))
+        .body("access_token", notNullValue())
+        .extract()
+          .path("access_token");
+    // @formatter:on
+
+    // Check audience is NOT encoded in JWT access token
+    try {
+      JWT jwtAccessToken = JWTParser.parse(actorAccessToken);
+      JWTClaimsSet claims = jwtAccessToken.getJWTClaimsSet();
+
+      assertThat(claims.getAudience(), is(nullValue()));
+
+    } catch (ParseException e) {
+      fail(e.getMessage());
+    }
+
+    // @formatter:off
+    // Introspect token
+    given()
+      .auth()
+        .preemptive()
+        .basic(actorClientId, actorClientSecret)
+      .port(iamPort)
+      .formParam("token", actorAccessToken)
+      .when()
+        .post("/introspect")
+      .then()
+        .log().body(true)
+        .statusCode(HttpStatus.OK.value())
+        .body("active", equalTo(true))
+        .body("scope", allOf(
+            containsString("read-tasks"),
+            containsString("openid"),
+            containsString("profile")))
+        .body("user_id", equalTo("test"))
+        .body("client_id", equalTo(actorClientId))
+        .body("aud", is(nullValue()));
+    // @formatter:on
+
+    // @formatter:off
+    // get user info on token, this fails as we did not 
+    // request to openid scope
+    given()
+      .port(iamPort)
+      .authentication()
+      .preemptive().oauth2(actorAccessToken)
+      .when()
+        .post("/userinfo")
+      .then()
+        .log().body(true)
+        .statusCode(HttpStatus.OK.value())
+        .body("sub", equalTo("80e5fb8d-b7c8-451a-89ba-346ae278a66f"));
     // @formatter:on
   }
 
   @Test
-  public void testClientWithoutGrantType() {
+  public void testUnauthorizedClient() {
 
     String clientId = "client-cred";
     String clientSecret = "secret";
 
     String audClientId = "tasks-app";
 
-    String accessToken = TestUtils.getAccessToken(clientId, clientSecret, "openid profile");
+    String accessToken = TestUtils.passwordTokenGetter(clientId, clientSecret)
+      .scope("openid profile")
+      .username("test")
+      .password("password")
+      .getAccessToken();
 
     // @formatter:off
     given()
@@ -110,7 +274,11 @@ public class TokenExchangeTests {
 
     String audClientId = "client";
 
-    String accessToken = TestUtils.getAccessToken(clientId, clientSecret, "openid offline_access");
+    String accessToken = TestUtils.passwordTokenGetter(clientId, clientSecret)
+      .scope("openid profile offline_access")
+      .username("test")
+      .password("password")
+      .getAccessToken();
 
     // @formatter:off
     // get refresh token
@@ -160,7 +328,7 @@ public class TokenExchangeTests {
   }
 
   @Test
-  public void testRequestRefreshTokenWithInvalidScope() {
+  public void testRequestRefreshTokenWithoutOfflineAccessScope() {
 
     String clientId = "token-exchange-subject";
     String clientSecret = "secret";
@@ -170,7 +338,11 @@ public class TokenExchangeTests {
 
     String audClientId = "client";
 
-    String accessToken = TestUtils.getAccessToken(clientId, clientSecret, "openid");
+    String accessToken = TestUtils.passwordTokenGetter(clientId, clientSecret)
+      .scope("openid")
+      .username("test")
+      .password("password")
+      .getAccessToken();
 
     // @formatter:off
     given()
@@ -204,12 +376,16 @@ public class TokenExchangeTests {
 
     String audClientId = "client";
 
-    String subjectToken = TestUtils.clientCredentialsTokenGetter(clientId, clientSecret)
+    String subjectToken = TestUtils.passwordTokenGetter(clientId, clientSecret)
       .scope("openid")
+      .username("test")
+      .password("password")
       .getAccessToken();
 
-    String actorToken = TestUtils.clientCredentialsTokenGetter(actorClientId, actorClientSecret)
+    String actorToken = TestUtils.passwordTokenGetter(clientId, clientSecret)
       .scope("openid")
+      .username("test")
+      .password("password")
       .getAccessToken();
 
     // @formatter:off
@@ -233,7 +409,7 @@ public class TokenExchangeTests {
         .body(true)
       .statusCode(HttpStatus.BAD_REQUEST.value())
       .body("error", equalTo("invalid_request"))
-      .body("error_description", Matchers.stringContainsInOrder(Arrays.asList("not yet supported")));
+      .body("error_description", Matchers.stringContainsInOrder(Arrays.asList("not supported")));
     // @formatter:on
   }
 
@@ -267,52 +443,13 @@ public class TokenExchangeTests {
   }
 
   @Test
-  public void testWithAudienceField() {
-    String clientId = "token-exchange-subject";
-    String clientSecret = "secret";
+  public void testTokenExchangeFailureForClientCredentialsClient() {
+
+    String accessToken =
+        TestUtils.clientCredentialsTokenGetter().scope("read-tasks write-tasks").getAccessToken();
 
     String actorClientId = "token-exchange-actor";
     String actorClientSecret = "secret";
-
-    String audClientId = "tasks-app";
-
-    String accessToken = TestUtils.getAccessToken(clientId, clientSecret, "openid profile");
-
-    // @formatter:off
-    given()
-      .auth()
-        .preemptive()
-          .basic(actorClientId, actorClientSecret)
-      .port(iamPort)
-      .param("grant_type", GRANT_TYPE)
-      .param("audience", audClientId)
-      .param("subject_token", accessToken)
-      .param("subject_token_type", TOKEN_TYPE)
-      .param("scope", "read-tasks")
-    .when()
-      .post("/token")
-    .then()
-      .log()
-        .body(true)
-      .statusCode(HttpStatus.OK.value())
-      .body("scope", equalTo("read-tasks"))
-      .body("issued_token_type", equalTo(TOKEN_TYPE))
-      .body("token_type", equalTo("Bearer"))
-      .body("access_token", notNullValue())
-      .body("audience", equalTo(audClientId))
-      ;
-    // @formatter:on
-  }
-
-  @Test
-  public void testWithoutAudience() {
-    String clientId = "token-exchange-subject";
-    String clientSecret = "secret";
-
-    String actorClientId = "token-exchange-actor";
-    String actorClientSecret = "secret";
-
-    String accessToken = TestUtils.getAccessToken(clientId, clientSecret, "openid profile");
 
     // @formatter:off
     given()
@@ -329,13 +466,11 @@ public class TokenExchangeTests {
     .then()
       .log()
         .body(true)
-      .statusCode(HttpStatus.OK.value())
-      .body("scope", equalTo("read-tasks"))
-      .body("issued_token_type", equalTo(TOKEN_TYPE))
-      .body("token_type", equalTo("Bearer"))
-      .body("access_token", notNullValue())
-      .body("audience", Matchers.isEmptyOrNullString() )
-      ;
+      .statusCode(HttpStatus.BAD_REQUEST.value())
+      .body("error", equalTo("invalid_request"))
+      .body("error_description", Matchers.stringContainsInOrder(Arrays.asList("No user identity linked to subject token.")));
     // @formatter:on
+
+
   }
 }
