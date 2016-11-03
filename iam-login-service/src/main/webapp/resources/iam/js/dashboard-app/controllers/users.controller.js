57c93b523125170e4e4a75f1ce29364153c8833b
enricovianello <maverick17586@gmail.com>
2016-11-03 09:19:56 +0100

    Users page hides columns on window resizing

--- iam-login-service/src/main/webapp/resources/iam/js/dashboard-app/controllers/users.controller.js
+++ iam-login-service/src/main/webapp/resources/iam/js/dashboard-app/controllers/users.controller.js
@@ -21,8 +21,6 @@ function UsersController($scope, $rootScope, $uibModal, $state, $filter, filterF
 	users.currentPage = 1;
 	users.entryLimit = 10; // items per page
 
-	$scope.promises = [];
-
 	// functions
 	users.resetFilters = resetFilters;
 	users.rebuildFilteredList = rebuildFilteredList;
@@ -39,6 +37,12 @@ function UsersController($scope, $rootScope, $uibModal, $state, $filter, filterF
 	function resetFilters() {
 		// needs to be a function or it won't trigger a $watch
 		users.searchText = "";
+	}
+
+	users.isPageLoaded = isPageLoaded;
+
+	function isPageLoaded() {
+		return $rootScope.usersLoadingProgress == 100;
 	}
 
 	function rebuildFilteredList() {
@@ -78,50 +82,47 @@ function UsersController($scope, $rootScope, $uibModal, $state, $filter, filterF
 
 	function loadUserList() {
 
-//		$rootScope.usersLoadingProgress = 0;
-//		users.loadingModal = $uibModal
-//		.open({
-//			animation: false,
-//			templateUrl : '/resources/iam/template/dashboard/users/loading-modal.html'
-//		});
+		$rootScope.usersLoadingProgress = 0;
+		users.loadingModal = $uibModal
+		.open({
+			animation: false,
+			templateUrl : '/resources/iam/template/dashboard/users/loading-modal.html'
+		});
 
-//		users.loadingModal.opened.then(function() {
+		users.loadingModal.opened.then(function() {
 			getAllUsers(1, users.entryLimit);
-//		});
+		});
 	}
 
 	function getAllUsers(startIndex, count) {
 
-		var p = scimFactory.getUsers(startIndex, count);
-		$scope.promises.push(p);
-		console.log($scope.promises);
-		p.then(
-						function(response) {
-							
-							angular.forEach(response.data.Resources, function(
-									user) {
-								users.list.push(user);
-							});
-							
-							users.rebuildFilteredList();
-							
-							if (response.data.totalResults > (response.data.startIndex - 1 + response.data.itemsPerPage)) {
-							
-								users.getAllUsers(startIndex + count, count);
-//								$rootScope.usersLoadingProgress = Math.floor((startIndex + count) * 100 / response.data.totalResults);
-							
-							} else {
-							
-//								$rootScope.usersLoadingProgress = 100;
-//								users.loadingModal.dismiss("Cancel");
-							
-							}
-						}, function(error) {
-							
-							users.loadingModal.dismiss("Error");
-							$scope.operationResult = Utils.buildErrorOperationResult(error);
-						
-						});
+		scimFactory.getUsers(startIndex, count).then(
+			function(response) {
+
+				angular.forEach(response.data.Resources, function(user) {
+					users.list.push(user);
+				});
+
+				users.rebuildFilteredList();
+
+				if (response.data.totalResults > (response.data.startIndex - 1 + response.data.itemsPerPage)) {
+
+					$rootScope.usersLoadingProgress = Math.floor((startIndex + count) * 100 / response.data.totalResults);
+					users.getAllUsers(startIndex + count, count);
+
+				} else {
+
+					$rootScope.usersLoadingProgress = 100;
+					users.loadingModal.dismiss("Cancel");
+
+				}
+
+			}, function(error) {
+
+				users.loadingModal.dismiss("Error");
+				$scope.operationResult = Utils.buildErrorOperationResult(error);
+
+			});
 	}
 
 	function openAddUserDialog() {
