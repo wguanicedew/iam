3a6db505cfd8010520aada6aeb529bf8b5f39031
Marco Caberletti <marco.caberletti@gmail.com>
2016-11-14 13:17:54 +0100

    Add discovery endpoint unit test.

---
+++ iam-login-service/src/test/java/it/infn/mw/iam/test/oauth/DiscoveryEndpointTests.java
@@ -0,0 +1,59 @@
+package it.infn.mw.iam.test.oauth;
+
+import java.util.Arrays;
+import java.util.Set;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mitre.discovery.web.DiscoveryEndpoint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.google.common.collect.Sets;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Response;
+
+import it.infn.mw.iam.IamLoginService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = IamLoginService.class)
+@WebIntegrationTest
+public class DiscoveryEndpointTests {
+
+  @Value("${server.port}")
+  private Integer iamPort;
+
+  private String endpoint = DiscoveryEndpoint.OPENID_CONFIGURATION_URL;
+
+  private Set<String> iamSupportedGrants = Sets.newLinkedHashSet(Arrays.asList("authorization_code",
+      "implicit", "refresh_token", "client_credentials", "password",
+      "urn:ietf:params:oauth:grant-type:jwt-bearer", "urn:ietf:params:oauth:grant_type:redelegate",
+      "urn:ietf:params:oauth:grant-type:token-exchange"));
+
+  @Test
+  public void testGrantTypesSupported() {
+
+    // @formatter:off
+    Response response =RestAssured.given()
+      .port(iamPort)
+    .when()
+      .post(endpoint)
+    .then()
+      .log()
+        .body(true)
+      .statusCode(HttpStatus.OK.value())
+      .body("grant_types_supported", Matchers.notNullValue())
+      .extract()
+        .response()
+      ;
+    // @formatter:on
+
+    Set<String> grantsSet = Sets.newLinkedHashSet(response.getBody().path("grant_types_supported"));
+    Assert.assertThat(Sets.difference(iamSupportedGrants, grantsSet), Matchers.empty());
+  }
+}
