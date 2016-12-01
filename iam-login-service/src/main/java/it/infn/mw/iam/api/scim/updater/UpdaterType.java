04222b7920ee48838a9611baede6c4d07991979b
enricovianello <maverick17586@gmail.com>
2016-12-01 11:22:26 +0100

    Improved updater type description

--- iam-login-service/src/main/java/it/infn/mw/iam/api/scim/updater/UpdaterType.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/api/scim/updater/UpdaterType.java
@@ -2,27 +2,27 @@ package it.infn.mw.iam.api.scim.updater;
 
 public enum UpdaterType {
 
-  ACCOUNT_REPLACE_GIVEN_NAME("Replace user's given name"),
-  ACCOUNT_REPLACE_FAMILY_NAME("Replace user's family name"),
-  ACCOUNT_REPLACE_EMAIL("Replace user's email"),
-  ACCOUNT_REPLACE_PASSWORD("Replace user's password"),
-  ACCOUNT_REPLACE_PICTURE("Replace user's picture"),
-  ACCOUNT_REPLACE_USERNAME("Replace user's username"),
-  ACCOUNT_REPLACE_ACTIVE("Replace user's active status"),
+  ACCOUNT_REPLACE_GIVEN_NAME("Replace given name of user"),
+  ACCOUNT_REPLACE_FAMILY_NAME("Replace family name of user"),
+  ACCOUNT_REPLACE_EMAIL("Replace email of user"),
+  ACCOUNT_REPLACE_PASSWORD("Replace password of user"),
+  ACCOUNT_REPLACE_PICTURE("Replace picture of user"),
+  ACCOUNT_REPLACE_USERNAME("Replace username of user"),
+  ACCOUNT_REPLACE_ACTIVE("Replace active status of user"),
 
   ACCOUNT_ADD_OIDC_ID("Add OpenID Connect account to user"),
   ACCOUNT_ADD_SAML_ID("Add SAML account to user"),
   ACCOUNT_ADD_SSH_KEY("Add ssh key to user"),
   ACCOUNT_ADD_X509_CERTIFICATE("Add x509 certificate to user"),
 
-  ACCOUNT_REMOVE_OIDC_ID("Remove user's OpenID Connect account"),
-  ACCOUNT_REMOVE_SAML_ID("Remove user's SAML account"),
-  ACCOUNT_REMOVE_SSH_KEY("Remove user's ssh key"),
-  ACCOUNT_REMOVE_X509_CERTIFICATE("Remove user's x509 certificate"),
+  ACCOUNT_REMOVE_OIDC_ID("Remove OpenID Connect account from user"),
+  ACCOUNT_REMOVE_SAML_ID("Remove SAML account from user"),
+  ACCOUNT_REMOVE_SSH_KEY("Remove ssh key from user"),
+  ACCOUNT_REMOVE_X509_CERTIFICATE("Remove x509 certificate from user"),
 
-  ACCOUNT_ADD_GROUP_MEMBERSHIP("Add group membership to user"),
-  ACCOUNT_REMOVE_GROUP_MEMBERSHIP("Remove user's group membership"), 
-  ACCOUNT_REMOVE_PICTURE("Remove user's picture");
+  ACCOUNT_ADD_GROUP_MEMBERSHIP("Add group to user"),
+  ACCOUNT_REMOVE_GROUP_MEMBERSHIP("Remove user from group"), 
+  ACCOUNT_REMOVE_PICTURE("Remove picture from user");
 
   private String description;
 
