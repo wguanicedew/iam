87b76564d74f4e35fd7237a87529ac8338a4a5ec
enricovianello <maverick17586@gmail.com>
2017-01-19 22:27:36 +0100

    Added missing equals

--- iam-login-service/src/main/java/it/infn/mw/iam/api/scim/model/ScimOidcId.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/api/scim/model/ScimOidcId.java
@@ -66,6 +66,37 @@ public class ScimOidcId {
   }
 
   @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((issuer == null) ? 0 : issuer.hashCode());
+    result = prime * result + ((subject == null) ? 0 : subject.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    ScimOidcId other = (ScimOidcId) obj;
+    if (issuer == null) {
+      if (other.issuer != null)
+        return false;
+    } else if (!issuer.equals(other.issuer))
+      return false;
+    if (subject == null) {
+      if (other.subject != null)
+        return false;
+    } else if (!subject.equals(other.subject))
+      return false;
+    return true;
+  }
+
+  @Override
   public String toString() {
     return "ScimOidcId [issuer=" + issuer + ", subject=" + subject + "]";
   }
--- iam-login-service/src/main/java/it/infn/mw/iam/api/scim/model/ScimSamlId.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/api/scim/model/ScimSamlId.java
@@ -60,6 +60,37 @@ public class ScimSamlId {
     return new Builder();
   }
 
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((idpId == null) ? 0 : idpId.hashCode());
+    result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    ScimSamlId other = (ScimSamlId) obj;
+    if (idpId == null) {
+      if (other.idpId != null)
+        return false;
+    } else if (!idpId.equals(other.idpId))
+      return false;
+    if (userId == null) {
+      if (other.userId != null)
+        return false;
+    } else if (!userId.equals(other.userId))
+      return false;
+    return true;
+  }
+
   public static class Builder {
 
     private String idpId;
