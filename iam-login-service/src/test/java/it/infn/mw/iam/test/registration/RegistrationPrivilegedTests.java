be5ad49f389b2d40db9b4c5967389fc9e65cf580
marcocaberletti <marco.caberletti@gmail.com>
2017-05-29 12:18:05 +0200

    Mandatory registration notes (#138)
    * Change registration notes from optional to mandatory.
    Refactor registration unit tests.
    
    * Fix registration access token lifetime setting
    
    * Plus tests that check that registration access tokens work as expected
    
    * A test that registers a new client and checks that the registration
    access token works.

---
+++ iam-login-service/src/test/java/it/infn/mw/iam/test/registration/RegistrationPrivilegedTests.java
@@ -0,0 +1,271 @@
+package it.infn.mw.iam.test.registration;
+
+import static it.infn.mw.iam.api.scim.model.ScimConstants.SCIM_CONTENT_TYPE;
+import static it.infn.mw.iam.core.IamRegistrationRequestStatus.APPROVED;
+import static it.infn.mw.iam.core.IamRegistrationRequestStatus.CONFIRMED;
+import static it.infn.mw.iam.core.IamRegistrationRequestStatus.NEW;
+import static it.infn.mw.iam.core.IamRegistrationRequestStatus.REJECTED;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.transaction.Transactional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import it.infn.mw.iam.IamLoginService;
+import it.infn.mw.iam.core.IamRegistrationRequestStatus;
+import it.infn.mw.iam.registration.PersistentUUIDTokenGenerator;
+import it.infn.mw.iam.registration.RegistrationRequestDto;
+import it.infn.mw.iam.test.core.CoreControllerTestSupport;
+import it.infn.mw.iam.test.util.WithMockOAuthUser;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {IamLoginService.class, CoreControllerTestSupport.class})
+@WebAppConfiguration
+@Transactional
+public class RegistrationPrivilegedTests {
+
+  @Autowired
+  private WebApplicationContext context;
+
+  @Autowired
+  private PersistentUUIDTokenGenerator generator;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  private MockMvc mvc;
+
+  @Before
+  public void setup() {
+    mvc = MockMvcBuilders.webAppContextSetup(context)
+      .apply(springSecurity())
+      .alwaysDo(print())
+      .build();
+  }
+
+  @Test
+  @WithMockOAuthUser(clientId = "registration-client", scopes = {"registration:read"})
+  public void testListNewRequests() throws Exception {
+
+    createRegistrationRequest("test_list_new");
+
+    // @formatter:off
+    mvc.perform(get("/registration/list")
+        .param("status", NEW.name()))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$", hasSize(1)));
+    // @formatter:on
+  }
+
+  @Test
+  @WithMockOAuthUser(clientId = "registration-client",
+      scopes = {"registration:read", "registration:write"})
+  public void testListPendingRequest() throws Exception {
+
+    createRegistrationRequest("test_1");
+    createRegistrationRequest("test_2");
+
+    String confirmationKey = generator.getLastToken();
+    confirmRegistrationRequest(confirmationKey);
+
+    RegistrationRequestDto reg3 = createRegistrationRequest("test_3");
+    approveRequest(reg3.getUuid());
+
+    // @formatter:off
+    // 1 NEW, 1 CONFIRMED, 1 APPROVED -> expect 2 elements returned
+    mvc.perform(get("/registration/list/pending"))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$", hasSize(2)));
+    // @formatter:on
+  }
+
+  @Test
+  @WithMockOAuthUser(clientId = "registration-client", scopes = {"registration:read"})
+  public void testListAllRequests() throws Exception {
+
+    createRegistrationRequest("test_list_1");
+    createRegistrationRequest("test_list_2");
+
+    String token = generator.getLastToken();
+    assertNotNull(token);
+
+    confirmRegistrationRequest(token);
+
+    // @formatter:off
+    mvc.perform(get("/registration/list"))
+     .andExpect(status().isOk())
+     .andExpect(jsonPath("$", hasSize(2)));
+    // @formatter:on
+  }
+
+  @Test
+  @WithMockOAuthUser(clientId = "registration-client", scopes = {"registration:write"})
+  public void testApproveRequest() throws Exception {
+
+    RegistrationRequestDto reg = createRegistrationRequest("test_approve");
+    assertNotNull(reg);
+
+    String token = generator.getLastToken();
+    assertNotNull(token);
+
+    confirmRegistrationRequest(token);
+
+    // approve it
+    // @formatter:off
+    mvc.perform(post("/registration/{uuid}/{decision}", reg.getUuid(), APPROVED.name()))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.status", equalTo(APPROVED.name())))
+      .andExpect(jsonPath("$.uuid", equalTo(reg.getUuid())));
+    // @formatter:on
+  }
+
+  @Test
+  @WithMockOAuthUser(clientId = "registration-client",
+      scopes = {"scim:write", "scim:read", "registration:write"})
+  public void testRejectRequest() throws Exception {
+
+    RegistrationRequestDto reg = createRegistrationRequest("test_reject");
+    assertNotNull(reg);
+
+    String token = generator.getLastToken();
+    assertNotNull(token);
+
+    confirmRegistrationRequest(token);
+
+    // @formatter:off
+    // reject it
+    mvc.perform(post("/registration/{uuid}/{decision}", reg.getUuid(), REJECTED.name()))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.status", equalTo(REJECTED.name())))
+      .andExpect(jsonPath("$.uuid", equalTo(reg.getUuid())));
+    // @formatter:on
+
+    // Reject delete user: verify user not found
+    // @formatter:off
+    mvc.perform(get("/scim/Users/{uuid}", reg.getAccountId())
+        .contentType(SCIM_CONTENT_TYPE))
+      .andExpect(status().isNotFound());
+    // @formatter:on
+  }
+
+  @Test
+  @WithMockOAuthUser(clientId = "registration-client", scopes = {"registration:write"})
+  public void testApproveRequestNotConfirmed() throws Exception {
+
+    // create new request
+    RegistrationRequestDto reg = createRegistrationRequest("test_approve_not_confirmed");
+    assertNotNull(reg);
+
+    // @formatter:off
+    // approve it without confirm
+    mvc.perform(post("/registration/{uuid}/{decision}", reg.getUuid(), APPROVED.name()))
+      .andExpect(status().isOk());
+    // @formatter:on
+  }
+
+  @Test
+  @WithMockOAuthUser(clientId = "registration-client", scopes = {"registration:write"})
+  public void testWrongDecisionFailure() throws Exception {
+
+    RegistrationRequestDto reg = createRegistrationRequest("test_wrong_decision");
+    assertNotNull(reg);
+
+    String token = generator.getLastToken();
+    assertNotNull(token);
+
+    confirmRegistrationRequest(token);
+
+    // @formatter:off
+    mvc.perform(post("/registration/{uuid}/{decision}", reg.getUuid(), "wrong"))
+      .andExpect(status().isBadRequest());
+    // @formatter:on
+  }
+
+  @Test
+  @WithMockOAuthUser(clientId = "registration-client", scopes = {"registration:write"})
+  public void testConfirmAfterApprovation() throws Exception {
+
+    RegistrationRequestDto reg = createRegistrationRequest("test_confirm_after_approve");
+    assertNotNull(reg);
+    String confirmationKey = generator.getLastToken();
+
+    approveRequest(reg.getUuid());
+
+    // @formatter:off
+    mvc.perform(get("/registration/confirm/{token}", confirmationKey))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.status", equalTo(APPROVED.name())));
+    // @formatter:on
+  }
+
+  private RegistrationRequestDto createRegistrationRequest(String username) throws Exception {
+
+    String email = username + "@example.org";
+    RegistrationRequestDto request = new RegistrationRequestDto();
+    request.setGivenname("Test");
+    request.setFamilyname("User");
+    request.setEmail(email);
+    request.setUsername(username);
+    request.setNotes("Some short notes...");
+    request.setPassword("password");
+
+    // @formatter:off
+    String response = mvc
+      .perform(post("/registration/create").contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(request)))
+      .andExpect(status().isOk())
+      .andReturn()
+      .getResponse()
+      .getContentAsString();
+    // @formatter:on
+
+    return objectMapper.readValue(response, RegistrationRequestDto.class);
+  }
+
+  private void confirmRegistrationRequest(String confirmationKey) throws Exception {
+    // @formatter:off
+    mvc.perform(get("/registration/confirm/{token}", confirmationKey))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.status", equalTo(CONFIRMED.name())));
+    // @formatter:on
+  }
+
+  protected RegistrationRequestDto approveRequest(String uuid) throws Exception {
+    return requestDecision(uuid, APPROVED);
+  }
+
+  private RegistrationRequestDto requestDecision(String uuid, IamRegistrationRequestStatus decision)
+      throws Exception {
+    // @formatter:off
+    String response = mvc
+        .perform(post("/registration/{uuid}/{decision}", uuid, decision.name())
+          .with(SecurityMockMvcRequestPostProcessors.user("admin").roles("ADMIN", "USER")))
+        .andExpect(status().isOk())
+        .andReturn()
+        .getResponse()
+        .getContentAsString();
+    // @formatter:on
+    return objectMapper.readValue(response, RegistrationRequestDto.class);
+  }
+}
