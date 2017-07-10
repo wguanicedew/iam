07544eae68b53a77036ec2fdcd02cc5f3db5251e
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2017-07-10 19:46:58 +0200

    Restore wait until port is free logic

--- iam-login-service/src/test/java/it/infn/mw/iam/test/util/MockSmtpServerUtils.java
+++ iam-login-service/src/test/java/it/infn/mw/iam/test/util/MockSmtpServerUtils.java
@@ -1,5 +1,7 @@
 package it.infn.mw.iam.test.util;
 
+import java.io.IOException;
+import java.net.Socket;
 import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
@@ -9,7 +11,38 @@ import org.subethamail.wiser.Wiser;
 public class MockSmtpServerUtils {
   public static final Logger LOG = LoggerFactory.getLogger(MockSmtpServerUtils.class);
 
+  private static void waitUntilPortIsFree(String host, int port, int timeoutInSecs) {
+
+    boolean portBusy = true;
+    int sleepTimeInSecs = 0;
+
+    do {
+      try {
+        // Open a client socket to talk with host at port
+        (new Socket(host, port)).close();
+
+        // If we reach this far there's someone listening,
+        // i.e. the port is busy
+        LOG.warn("Port {} already used", port);
+
+        // So sleep some time
+        Thread.sleep(TimeUnit.SECONDS.toMillis(1));
+        sleepTimeInSecs++;
+        portBusy = true;
+      } catch (IOException e) {
+        portBusy = false;
+      } catch (InterruptedException e) {
+      }
+    } while (portBusy && sleepTimeInSecs < timeoutInSecs);
+
+    if (sleepTimeInSecs >= timeoutInSecs) {
+      throw new AssertionError("Timeout reached waiting for port " + port);
+    }
+  }
+
   public static synchronized Wiser startMockSmtpServer(String hostname, int port) {
+
+    waitUntilPortIsFree(hostname, port, 5);
 
     LOG.info("Starting Wiser SMTP server on: {}:{}", hostname, port);
 
@@ -17,7 +50,7 @@ public class MockSmtpServerUtils {
 
     mockSmtpServer.setHostname(hostname);
     mockSmtpServer.setPort(port);
-    
+
     mockSmtpServer.start();
 
     return mockSmtpServer;
@@ -28,10 +61,6 @@ public class MockSmtpServerUtils {
     if (mockSmtpServer != null) {
       LOG.info("Stopping Wiser SMTP server");
       mockSmtpServer.stop();
-      try {
-        Thread.currentThread().sleep(TimeUnit.SECONDS.toMillis(1));
-      } catch (InterruptedException e) {
-      }
     }
 
   }
