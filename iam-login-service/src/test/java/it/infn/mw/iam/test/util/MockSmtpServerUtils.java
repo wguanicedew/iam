e7b0e7469dab4e336c83a56b7b5d98d765281749
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2017-07-10 18:55:57 +0200

    Sleep 1 sec after server shutdown

--- iam-login-service/src/test/java/it/infn/mw/iam/test/util/MockSmtpServerUtils.java
+++ iam-login-service/src/test/java/it/infn/mw/iam/test/util/MockSmtpServerUtils.java
@@ -1,5 +1,7 @@
 package it.infn.mw.iam.test.util;
 
+import java.util.concurrent.TimeUnit;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.subethamail.wiser.Wiser;
@@ -10,19 +12,27 @@ public class MockSmtpServerUtils {
   public static synchronized Wiser startMockSmtpServer(String hostname, int port) {
 
     LOG.info("Starting Wiser SMTP server on: {}:{}", hostname, port);
-    
+
     Wiser mockSmtpServer = new Wiser();
-    
+
     mockSmtpServer.setHostname(hostname);
     mockSmtpServer.setPort(port);
-
+    
     mockSmtpServer.start();
 
     return mockSmtpServer;
   }
 
   public static synchronized void stopMockSmtpServer(Wiser mockSmtpServer) {
-    LOG.info("Stopping Wiser SMTP server");
-    mockSmtpServer.stop();
+
+    if (mockSmtpServer != null) {
+      LOG.info("Stopping Wiser SMTP server");
+      mockSmtpServer.stop();
+      try {
+        Thread.currentThread().sleep(TimeUnit.SECONDS.toMillis(1));
+      } catch (InterruptedException e) {
+      }
+    }
+
   }
 }
