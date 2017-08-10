b37edaf0adab9f661f5f130e357cfcab2d0ccb10
enricovianello <maverick17586@gmail.com>
2017-08-10 10:21:31 +0200

    Fixed Sonar issues

--- iam-login-service/src/main/java/it/infn/mw/iam/api/tokens/Constants.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/api/tokens/Constants.java
@@ -2,10 +2,10 @@ package it.infn.mw.iam.api.tokens;
 
 public class Constants {
 
+  public static final String ACCESS_TOKENS_ENDPOINT = "/iam/api/access-tokens";
+  public static final String REFRESH_TOKENS_ENDPOINT = "/iam/api/refresh-tokens";
+
   private Constants() {
     throw new IllegalStateException("Utility class");
   }
-
-  public static final String ACCESS_TOKENS_ENDPOINT = "/iam/api/access-tokens";
-  public static final String REFRESH_TOKENS_ENDPOINT = "/iam/api/refresh-tokens";
 }
