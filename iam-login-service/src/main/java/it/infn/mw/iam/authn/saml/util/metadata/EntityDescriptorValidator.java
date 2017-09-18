9b80a7da8adb562ca50257afc843a39808562a35
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2017-09-18 17:12:24 +0200

    Remove unused interface

--- iam-login-service/src/main/java/it/infn/mw/iam/authn/saml/util/metadata/EntityDescriptorValidator.java
+++
@@ -1,8 +0,0 @@
-package it.infn.mw.iam.authn.saml.util.metadata;
-
-import org.opensaml.saml2.metadata.EntityDescriptor;
-
-public interface EntityDescriptorValidator {
-
-  public ValidationResult validateEntityDescriptor(EntityDescriptor descriptor);
-}
