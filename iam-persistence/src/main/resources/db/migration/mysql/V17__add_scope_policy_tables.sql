9850ce3c532cd372817b8d8afde4e20d5be8b9f0
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2017-09-21 09:36:47 +0200

    Fix MySQL scope policies table definition

--- iam-persistence/src/main/resources/db/migration/mysql/V17__add_scope_policy_tables.sql
+++ iam-persistence/src/main/resources/db/migration/mysql/V17__add_scope_policy_tables.sql
@@ -1,21 +1,27 @@
-CREATE TABLE iam_scope (ID BIGINT AUTO_INCREMENT NOT NULL, SCOPE VARCHAR(256) NOT NULL UNIQUE, PRIMARY KEY (ID));
-CREATE TABLE iam_scope_policy (ID BIGINT AUTO_INCREMENT NOT NULL, creation_time DATETIME NOT NULL, 
-  description VARCHAR(512), last_update_time DATETIME NOT NULL, rule VARCHAR(6) NOT NULL, 
-  account_id BIGINT, group_id BIGINT, PRIMARY KEY (ID));
-CREATE TABLE iam_scope_policy_scope (policy_id BIGINT NOT NULL, scope_id BIGINT NOT NULL, 
-  PRIMARY KEY (policy_id, scope_id));
+CREATE TABLE iam_scope_policy (ID BIGINT AUTO_INCREMENT NOT NULL, 
+  creation_time DATETIME NOT NULL, 
+  description VARCHAR(512), 
+  last_update_time DATETIME NOT NULL, 
+  rule VARCHAR(6) NOT NULL, 
+  account_id BIGINT, 
+  group_id BIGINT, 
+  PRIMARY KEY (ID));
   
-ALTER TABLE iam_scope_policy 
-  ADD CONSTRAINT FK_iam_scope_policy_group_id FOREIGN KEY (group_id) REFERENCES iam_group (ID);
+CREATE TABLE iam_scope_policy_scope (policy_id BIGINT, scope VARCHAR(256));
 
-ALTER TABLE iam_scope_policy 
-  ADD CONSTRAINT FK_iam_scope_policy_account_id FOREIGN KEY (account_id) REFERENCES iam_account (ID);
+CREATE UNIQUE INDEX INDEX_iam_scope_policy_scope_policy_id_scope 
+  ON iam_scope_policy_scope (policy_id, scope);
 
-ALTER TABLE iam_scope_policy_scope 
-  ADD CONSTRAINT FK_iam_scope_policy_scope_policy_id FOREIGN KEY (policy_id) REFERENCES iam_scope_policy (ID);
+CREATE INDEX INDEX_iam_scope_policy_scope_scope ON iam_scope_policy_scope (scope);
 
-ALTER TABLE iam_scope_policy_scope 
-  ADD CONSTRAINT FK_iam_scope_policy_scope_scope_id FOREIGN KEY (scope_id) REFERENCES iam_scope (ID);
+ALTER TABLE iam_scope_policy ADD CONSTRAINT FK_iam_scope_policy_group_id 
+  FOREIGN KEY (group_id) REFERENCES iam_group (ID);
+
+ALTER TABLE iam_scope_policy ADD CONSTRAINT FK_iam_scope_policy_account_id 
+  FOREIGN KEY (account_id) REFERENCES iam_account (ID);
+
+ALTER TABLE iam_scope_policy_scope ADD CONSTRAINT FK_iam_scope_policy_scope_policy_id 
+  FOREIGN KEY (policy_id) REFERENCES iam_scope_policy (ID);
 
 -- insert default policy (allow all scopes)
 INSERT INTO iam_scope_policy(id, creation_time, description, last_update_time, rule) 
