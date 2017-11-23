21d3e5794cac61f7f94c0e80424ab6c47a4c492a
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2017-11-23 15:45:10 +0100

    travis: use wrapper script to work around log limit

---
+++ travis/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Freely adapted from https://stackoverflow.com/questions/26082444/how-to-work-around-travis-cis-4mb-output-limit
+set -e
+
+export PING_SLEEP=30s
+export BUILD_OUTPUT=travis/travis-build.out
+
+dump_output() {
+   echo Tailing the last 1000 lines of output:
+   tail -1000 $BUILD_OUTPUT
+}
+
+error_handler() {
+  echo ERROR: An error was encountered with the build.
+  dump_output
+  exit 1
+}
+
+trap 'error_handler' ERR
+
+bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
+PING_LOOP_PID=$!
+
+mvn clean package >> $BUILD_OUTPUT 2>&1
+dump_output
+kill ${PING_LOOP_PID}
