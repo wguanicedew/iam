0d16e3fa85ec4045a218faae1e968d999f74439a
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2017-11-27 11:42:12 +0100

    mitre: fix typo

--- travis/install-mitreid.sh
+++ travis/install-mitreid.sh
@@ -3,7 +3,7 @@ set -ex
 
 MITREID_CNAF_REPO=${MITREID_CNAF_REPO:-https://github.com/indigo-iam/OpenID-Connect-Java-Spring-Server.git}
 MITREID_CNAF_BRANCH=${MITREID_CNAF_BRANCH:-develop-1.3.x}
-BUILD_LOG=travis-mitre-build.out
+BUILD_LOG=/tmp/travis-mitre-build.out
 
 error_handler() {
   echo ERROR: An error was encountered bulding mitreid 
@@ -15,5 +15,5 @@ trap 'error_handler' ERR
 git clone ${MITREID_CNAF_REPO} ${HOME}/mitreid-cnaf
 pushd ${HOME}/mitreid-cnaf
 git checkout ${MITREID_CNAF_BRANCH}
-mvn install >${BUILD_LOG} &2>1 || error_handler
+mvn install > ${BUILD_LOG} 2>&1 || error_handler
 popd
