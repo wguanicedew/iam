e868ee871fead3561c5a950751f2634da8d280f0
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2017-11-27 11:18:47 +0100

    travis: mitre install in dedicated script

---
+++ travis/install-mitreid.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+MITREID_CNAF_REPO=${MITREID_CNAF_REPO:-https://github.com/indigo-iam/OpenID-Connect-Java-Spring-Server.git}
+MITREID_CNAF_BRANCH=${MITREID_CNAF_BRANCH:-develop-1.3.x}
+BUILD_LOG=travis-mitre-build.out
+
+error_handler() {
+  echo ERROR: An error was encountered bulding mitreid 
+  tail -1000 ${BUILD_LOG}
+  exit 1
+}
+
+trap 'error_handler' ERR
+git clone ${MITREID_CNAF_REPO} ${HOME}/mitreid-cnaf
+pushd ${HOME}/mitreid-cnaf
+git checkout ${MITREID_CNAF_BRANCH}
+mvn install >${BUILD_LOG} &2>1 
+popd
