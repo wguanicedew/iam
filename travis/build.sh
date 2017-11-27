c341ac017fed52f69a012540690d7c4df0bf72a6
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2017-11-27 12:39:14 +0100

    travis: leverage docker-compose to run mysql tests

--- travis/build.sh
+++ travis/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Freely adapted from https://stackoverflow.com/questions/26082444/how-to-work-around-travis-cis-4mb-output-limit
-set -e
+set -ex
 
 export PING_SLEEP=30s
 export BUILD_OUTPUT=travis/travis-build.out
@@ -21,6 +21,10 @@ trap 'error_handler' ERR
 bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
 PING_LOOP_PID=$!
 
+docker-compose up -d db
 mvn clean package >> $BUILD_OUTPUT 2>&1
+echo "IAM H2 build & test completed succesfully"
+echo "--> MySQL build & test" >> $BUILD_OUTPUT
+IAM_DB_HOST=127.0.0.1 mvn -Dspring.profiles.active=mysql-test >> $BUILD_OUTPUT 2>&1
 dump_output
 kill ${PING_LOOP_PID}
