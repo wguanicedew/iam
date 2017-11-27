83e08f21f49b4a674bc26c565269c847cb8af1d4
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2017-11-27 12:09:45 +0100

    travis: more verbose success logging

--- travis/install-mitreid.sh
+++ travis/install-mitreid.sh
@@ -3,17 +3,20 @@ set -ex
 
 MITREID_CNAF_REPO=${MITREID_CNAF_REPO:-https://github.com/indigo-iam/OpenID-Connect-Java-Spring-Server.git}
 MITREID_CNAF_BRANCH=${MITREID_CNAF_BRANCH:-develop-1.3.x}
-BUILD_LOG=/tmp/travis-mitre-build.out
+MITREID_BUILD_DIR=/tmp/mitreid-cnaf
+MITREID_BUILD_LOG=/tmp/travis-mitre-build.out
 
 error_handler() {
   echo ERROR: An error was encountered bulding mitreid 
-  tail -1000 ${BUILD_LOG}
+  tail -1000 ${MITREID_BUILD_LOG}
   exit 1
 }
 
 trap 'error_handler' ERR
-git clone ${MITREID_CNAF_REPO} ${HOME}/mitreid-cnaf
-pushd ${HOME}/mitreid-cnaf
+git clone ${MITREID_CNAF_REPO} ${MITREID_BUILD_DIR} 
+pushd ${MITREID_BUILD_DIR}
 git checkout ${MITREID_CNAF_BRANCH}
-mvn install > ${BUILD_LOG} 2>&1 || error_handler
+mvn install > ${MITREID_BUILD_LOG} 2>&1
+echo "MitreID build completed succesfully"
+tail -100 ${MITREID_BUILD_LOG}
 popd
