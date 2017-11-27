681cabf73cf4f3162f19755e5e870d572c71a6d0
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2017-11-27 11:27:54 +0100

    travis: more verbose mitre installation

--- travis/install-mitreid.sh
+++ travis/install-mitreid.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ex
 
 MITREID_CNAF_REPO=${MITREID_CNAF_REPO:-https://github.com/indigo-iam/OpenID-Connect-Java-Spring-Server.git}
 MITREID_CNAF_BRANCH=${MITREID_CNAF_BRANCH:-develop-1.3.x}
@@ -15,5 +15,5 @@ trap 'error_handler' ERR
 git clone ${MITREID_CNAF_REPO} ${HOME}/mitreid-cnaf
 pushd ${HOME}/mitreid-cnaf
 git checkout ${MITREID_CNAF_BRANCH}
-mvn install >${BUILD_LOG} &2>1 
+mvn install >${BUILD_LOG} &2>1 || error_handler
 popd
