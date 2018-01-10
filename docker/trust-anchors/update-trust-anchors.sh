22977d8373d725c5aacc982f79d1cf8e90f58ea9
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2018-01-10 16:48:33 +0100

    docker: add ability to skip trust-anchors update

--- docker/trust-anchors/update-trust-anchors.sh
+++ docker/trust-anchors/update-trust-anchors.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -ex
 
+if [ -n "${SKIP_UPDATE_TRUST_ANCHORS}" ]; then
+  echo "Skipping trust anchors update as requested."
+  exit 0
+fi
+
 fetch-crl --verbose || true
 
 echo "Args: $@"
