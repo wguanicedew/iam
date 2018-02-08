b998919b4f9e02f44b3bf1538a9425ec5331b56c
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2018-02-08 18:10:23 +0100

    More SonarQube fixes

--- iam-login-service/src/main/java/it/infn/mw/iam/audit/events/aup/AupDeletedEvent.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/audit/events/aup/AupDeletedEvent.java
@@ -4,9 +4,13 @@ import it.infn.mw.iam.persistence.model.IamAup;
 
 public class AupDeletedEvent extends AupEvent {
 
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
   public AupDeletedEvent(Object source, IamAup aup) {
     super(source, "AUP deleted", aup);
-    // TODO Auto-generated constructor stub
   }
 
 }
--- iam-login-service/src/main/java/it/infn/mw/iam/audit/utils/IamAupSignatureSerializer.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/audit/utils/IamAupSignatureSerializer.java
@@ -6,7 +6,6 @@ import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
 
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
@@ -18,7 +17,7 @@ public class IamAupSignatureSerializer extends JsonSerializer<IamAupSignature> {
 
   @Override
   public void serialize(IamAupSignature value, JsonGenerator gen, SerializerProvider serializers)
-      throws IOException, JsonProcessingException {
+      throws IOException {
 
     gen.writeStartObject();
     gen.writeNumberField("aupId", value.getAup().getId());
