15670382257d21f7e6b63649e46fcb0650b286ef
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2018-04-12 18:59:12 +0200

    Remove license from sql migrations
    To avoid flyway upgrade issues

--- iam-persistence/src/main/resources/db/migration/mysql/V7___notification_tables.sql
+++ iam-persistence/src/main/resources/db/migration/mysql/V7___notification_tables.sql
@@ -1,19 +1,3 @@
---
--- Copyright (c) Istituto Nazionale di Fisica Nucleare (INFN). 2016-2018
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---     http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
---
-
 CREATE TABLE iam_email_notification (ID BIGINT AUTO_INCREMENT NOT NULL, UUID VARCHAR(36) NOT NULL UNIQUE, NOTIFICATION_TYPE VARCHAR(128) NOT NULL, SUBJECT VARCHAR(128), BODY TEXT, CREATION_TIME TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, DELIVERY_STATUS VARCHAR(128), LAST_UPDATE TIMESTAMP NULL DEFAULT NULL, REQUEST_ID BIGINT, PRIMARY KEY (ID));
 ALTER TABLE iam_email_notification ADD CONSTRAINT FK_iam_email_notification_request_id FOREIGN KEY (request_id) REFERENCES iam_reg_request (id);
 CREATE TABLE iam_notification_receiver(ID BIGINT AUTO_INCREMENT NOT NULL, NOTIFICATION_ID BIGINT, EMAIL_ADDRESS VARCHAR(254), PRIMARY KEY (ID));
