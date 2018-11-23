8d8e0c129c510930c4d5abbb54c79dde2c45f684
enricovianello <maverick17586@gmail.com>
2018-11-23 16:17:59 +0100

    Changed test username

--- iam-persistence/src/main/resources/db/migration/test/V100000_2___test_data_orphan_tokens.sql
+++ iam-persistence/src/main/resources/db/migration/test/V100000_2___test_data_orphan_tokens.sql
@@ -15,10 +15,10 @@ INSERT INTO AUTHENTICATION_HOLDER_EXTENSION (OWNER_ID, EXTENSION, VAL) VALUES
 INSERT INTO AUTHENTICATION_HOLDER_REQUEST_PARAMETER (OWNER_ID, PARAM, VAL) VALUES
 (1, 'scope', 'openid profile offline_access'),
 (1, 'grant_type', 'password'),
-(1, 'username', 'not-a-test-user');
+(1, 'username', 'vianello');
 
 INSERT INTO SAVED_USER_AUTH (ID, NAME, AUTHENTICATED, SOURCE_CLASS) VALUES
-(1, 'not-a-test-user', TRUE, 'org.springframework.security.authentication.UsernamePasswordAuthenticationToken');
+(1, 'vianello', TRUE, 'org.springframework.security.authentication.UsernamePasswordAuthenticationToken');
 
 INSERT INTO ACCESS_TOKEN (ID, TOKEN_VALUE, EXPIRATION, TOKEN_TYPE, REFRESH_TOKEN_ID, CLIENT_ID, AUTH_HOLDER_ID, ID_TOKEN_ID, APPROVED_SITE_ID) VALUES
 (1, 'eyJraWQiOiJyc2ExIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiI4MGU1ZmI4ZC1iN2M4LTQ1MWEtODliYS0zNDZhZTI3OGE2NmYiLCJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODA4MFwvIiwiZXhwIjoxNTQyODEwNzYwLCJpYXQiOjE1NDI4MDcxNjAsImp0aSI6ImEyNTJjMmE5LWFhZTEtNDNmZi04ZjNlLTAwY2JkODU0MTUwYSJ9.hm5GhHd1FOeeUkndGFKL8r8rNcpcmS_XFDyB6a4LFUO9uLqhC08-d1qDkpesg6MKTeTBuygA4ihX6khc8PGdfZRAtfQiYQJNqGJ72nOKZ-MNKBNqn0ztVEXvu9QTeGMKSyhFSOc2sScclZtCwUCIQfFXJsa_XjzpRdE_DuYOP0w', TIMESTAMPADD('DAY', 1, CURRENT_TIMESTAMP()), 'Bearer', 1, 9, 1, NULL, NULL);
