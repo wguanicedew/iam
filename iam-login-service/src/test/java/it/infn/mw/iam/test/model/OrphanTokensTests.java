d4fb286d6e78641740d667fc0165f3f4eb132bd4
enricovianello <maverick17586@gmail.com>
2018-11-26 12:35:18 +0100

    Fixed migration scripts for MySQL and removed excess tests

--- iam-login-service/src/test/java/it/infn/mw/iam/test/model/OrphanTokensTests.java
+++ iam-login-service/src/test/java/it/infn/mw/iam/test/model/OrphanTokensTests.java
@@ -18,14 +18,8 @@ package it.infn.mw.iam.test.model;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
-import java.util.List;
-import java.util.UUID;
-
-import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mitre.oauth2.model.OAuth2AccessTokenEntity;
-import org.mitre.oauth2.model.OAuth2RefreshTokenEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,10 +29,6 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import it.infn.mw.iam.IamLoginService;
-import it.infn.mw.iam.core.user.IamAccountService;
-import it.infn.mw.iam.persistence.model.IamAccount;
-import it.infn.mw.iam.persistence.model.IamUserInfo;
-import it.infn.mw.iam.persistence.repository.IamAccountRepository;
 import it.infn.mw.iam.persistence.repository.IamOAuthAccessTokenRepository;
 import it.infn.mw.iam.persistence.repository.IamOAuthRefreshTokenRepository;
 import it.infn.mw.iam.test.api.tokens.TestTokensUtils;
@@ -52,149 +42,15 @@ public class OrphanTokensTests extends TestTokensUtils {
 
   public static final Logger LOG = LoggerFactory.getLogger(OrphanTokensTests.class);
 
-  private static final String ORPHAN_USERNAME = "hpotter";
-  private static final String ORPHAN_GIVENNAME = "Harry";
-  private static final String ORPHAN_FAMILYNAME = "Potter";
-
-  @Autowired
-  private IamAccountService accountService;
   @Autowired
   private IamOAuthAccessTokenRepository accessTokensRepo;
   @Autowired
   private IamOAuthRefreshTokenRepository refreshTokensRepo;
-  @Autowired
-  private IamAccountRepository accountRepo;
-
-  private IamAccount newAccount(String givenName, String familyName, String username) {
-    IamAccount account = new IamAccount();
-    account.setUsername(username);
-    account.setUuid(UUID.randomUUID().toString());
-    IamUserInfo userInfo = new IamUserInfo();
-    userInfo.setEmail(username + "@email.com");
-    userInfo.setGivenName(givenName);
-    userInfo.setFamilyName(familyName);
-    userInfo.setIamAccount(account);
-    account.setUserInfo(userInfo);
-    return account;
-  }
-
-  private OAuth2AccessTokenEntity createOrphanAccessToken() {
-
-    final String TEST_CLIENT_ID = "token-lookup-client";
-    final String[] SCOPES = {"openid", "profile"};
-    IamAccount orphan = newAccount(ORPHAN_GIVENNAME, ORPHAN_FAMILYNAME, ORPHAN_USERNAME);
-    LOG.info("Creating new account {}", orphan);
-    accountService.createAccount(orphan);
-    LOG.info("Account exists? {}", accountRepo.findByUsername(ORPHAN_USERNAME).isPresent());
-    OAuth2AccessTokenEntity token = buildAccessToken(loadTestClient(TEST_CLIENT_ID), orphan.getUsername(), SCOPES);
-    LOG.info("Token name: {}", token.getAuthenticationHolder().getAuthentication().getName());
-    LOG.info("Created access token with id {}", accessTokensRepo.findAll().iterator().next().getId());
-    LOG.info("Deleting account ...");
-    accountRepo.delete(orphan);
-    LOG.info("Account exists? {}", accountRepo.findByUsername(ORPHAN_USERNAME).isPresent());
-    return token;
-  }
-
-  private OAuth2RefreshTokenEntity createOrphanRefreshToken() {
-
-    final String TEST_CLIENT_ID = "token-lookup-client";
-    final String[] SCOPES = {"openid", "profile", "offline_access"};
-    IamAccount orphan = newAccount(ORPHAN_GIVENNAME, ORPHAN_FAMILYNAME, ORPHAN_USERNAME);
-    LOG.info("Creating new account {}", orphan);
-    accountService.createAccount(orphan);
-    LOG.info("Account exists? {}", accountRepo.findByUsername(ORPHAN_USERNAME).isPresent());
-    OAuth2AccessTokenEntity token = buildAccessToken(loadTestClient(TEST_CLIENT_ID), orphan.getUsername(), SCOPES);
-    LOG.info("Created access token with id {}", accessTokensRepo.findAll().iterator().next().getId());
-    LOG.info("with refresh token with id {}", refreshTokensRepo.findAll().iterator().next().getId());
-    LOG.info("Deleting account ...");
-    accountRepo.delete(orphan);
-    return token.getRefreshToken();
-  }
-
-  @After
-  public void teardown() {
-
-    clearAllTokens();
-  }
 
   @Test
   public void assumeNoOrphanTokenIsFoundAtStart() {
 
     assertThat(accessTokensRepo.findOrphanedTokens().size(), equalTo(0));
-    assertThat(refreshTokensRepo.findOrphanedTokens().size(), equalTo(0));
-    assertThat(accessTokensRepo.count(), equalTo(0L));
-    assertThat(refreshTokensRepo.count(), equalTo(0L));
-  }
-
-  @Test
-  public void findAndDeleteOrphanAccessTokensTest() {
-
-    clearAllTokens();
-
-    /*
-     * Steps to create an orphan token: - Create a test user - build a valid access token - remove
-     * only the user
-     */
-    createOrphanAccessToken();
-
-    assertThat(accessTokensRepo.count(), equalTo(1L));
-    assertThat(accessTokensRepo.findAll()
-      .iterator()
-      .next()
-      .getAuthenticationHolder()
-      .getAuthentication()
-      .getName(), equalTo(ORPHAN_USERNAME));
-
-    assertThat(accountRepo.findByUsername(ORPHAN_USERNAME).isPresent(), equalTo(false));
-
-    List<OAuth2AccessTokenEntity> orphanTokens =
-        accessTokensRepo.findOrphanedTokens();
-
-    assertThat(orphanTokens.size(), equalTo(1));
-
-    assertThat(orphanTokens.get(0).getAuthenticationHolder().getAuthentication().getName(),
-        equalTo(ORPHAN_USERNAME));
-
-    accessTokensRepo.delete(orphanTokens);
-
-    assertThat(accessTokensRepo.findOrphanedTokens().size(), equalTo(0));
-  }
-
-  @Test
-  public void findAndDeleteOrphanRefreshTokensTest() {
-
-    clearAllTokens();
-
-    /*
-     * Steps to create an orphan token: - Create a test user - build a valid access token - remove
-     * only the user
-     */
-    createOrphanRefreshToken();
-
-    /* check access-token exists and it's owned by test user */
-    assertThat(refreshTokensRepo.count(), equalTo(1L));
-    assertThat(refreshTokensRepo.findAll()
-      .iterator()
-      .next()
-      .getAuthenticationHolder()
-      .getAuthentication()
-      .getName(), equalTo(ORPHAN_USERNAME));
-
-    /* find orphan tokens */
-    List<OAuth2RefreshTokenEntity> orphanTokens =
-        refreshTokensRepo.findOrphanedTokens();
-
-    assertThat(orphanTokens.size(), equalTo(1));
-
-    /* check orphan owner is the test user */
-    assertThat(orphanTokens.get(0).getAuthenticationHolder().getAuthentication().getName(),
-        equalTo(ORPHAN_USERNAME));
-    /* check test user doesn't exist */
-    assertThat(accountRepo.findByUsername(ORPHAN_USERNAME).isPresent(), equalTo(false));
-
-    /* remove orphan tokens */
-    refreshTokensRepo.delete(orphanTokens);
-
     assertThat(refreshTokensRepo.findOrphanedTokens().size(), equalTo(0));
   }
 }
--- iam-persistence/src/main/resources/db/migration/test/V100000_2___test_data_orphan_tokens.sql
+++ iam-persistence/src/main/resources/db/migration/test/V100000_2___test_data_orphan_tokens.sql
@@ -1,27 +1,27 @@
-INSERT INTO AUTHENTICATION_HOLDER (ID, USER_AUTH_ID, APPROVED, REDIRECT_URI, CLIENT_ID) VALUES
+INSERT INTO authentication_holder (id, user_auth_id, approved, redirect_uri, client_id) VALUES
 (1, 1, TRUE, NULL, 'token-exchange-subject');
 
-INSERT INTO SAVED_USER_AUTH_AUTHORITY (OWNER_ID, AUTHORITY) VALUES
+INSERT INTO saved_user_auth_authority (owner_id, authority) VALUES
 (1, 'ROLE_USER');
 
-INSERT INTO AUTHENTICATION_HOLDER_SCOPE (OWNER_ID, SCOPE) VALUES
+INSERT INTO authentication_holder_scope (owner_id, scope) VALUES
 (1, 'openid'),
 (1, 'offline_access'),
 (1, 'profile');
 
-INSERT INTO AUTHENTICATION_HOLDER_EXTENSION (OWNER_ID, EXTENSION, VAL) VALUES
-(1, 'AUTH_TIMESTAMP', TIMESTAMPADD('DAY', 1, CURRENT_TIMESTAMP()));
+INSERT INTO authentication_holder_extension (owner_id, extension, VAL) VALUES
+(1, 'AUTH_TIMESTAMP', TIMESTAMPADD(DAY, 1, CURRENT_TIMESTAMP()));
 
-INSERT INTO AUTHENTICATION_HOLDER_REQUEST_PARAMETER (OWNER_ID, PARAM, VAL) VALUES
+INSERT INTO authentication_holder_request_parameter (owner_id, param, val) VALUES
 (1, 'scope', 'openid profile offline_access'),
 (1, 'grant_type', 'password'),
 (1, 'username', 'vianello');
 
-INSERT INTO SAVED_USER_AUTH (ID, NAME, AUTHENTICATED, SOURCE_CLASS) VALUES
+INSERT INTO saved_user_auth (id, name, authenticated, source_class) VALUES
 (1, 'vianello', TRUE, 'org.springframework.security.authentication.UsernamePasswordAuthenticationToken');
 
-INSERT INTO ACCESS_TOKEN (ID, TOKEN_VALUE, EXPIRATION, TOKEN_TYPE, REFRESH_TOKEN_ID, CLIENT_ID, AUTH_HOLDER_ID, ID_TOKEN_ID, APPROVED_SITE_ID) VALUES
-(1, 'eyJraWQiOiJyc2ExIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiI4MGU1ZmI4ZC1iN2M4LTQ1MWEtODliYS0zNDZhZTI3OGE2NmYiLCJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODA4MFwvIiwiZXhwIjoxNTQyODEwNzYwLCJpYXQiOjE1NDI4MDcxNjAsImp0aSI6ImEyNTJjMmE5LWFhZTEtNDNmZi04ZjNlLTAwY2JkODU0MTUwYSJ9.hm5GhHd1FOeeUkndGFKL8r8rNcpcmS_XFDyB6a4LFUO9uLqhC08-d1qDkpesg6MKTeTBuygA4ihX6khc8PGdfZRAtfQiYQJNqGJ72nOKZ-MNKBNqn0ztVEXvu9QTeGMKSyhFSOc2sScclZtCwUCIQfFXJsa_XjzpRdE_DuYOP0w', TIMESTAMPADD('DAY', 1, CURRENT_TIMESTAMP()), 'Bearer', 1, 9, 1, NULL, NULL);
+INSERT INTO access_token (id, token_value, expiration, token_type, refresh_token_id, client_id, auth_holder_id, id_token_id, approved_site_id) VALUES
+(1, 'eyJraWQiOiJyc2ExIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiI4MGU1ZmI4ZC1iN2M4LTQ1MWEtODliYS0zNDZhZTI3OGE2NmYiLCJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODA4MFwvIiwiZXhwIjoxNTQyODEwNzYwLCJpYXQiOjE1NDI4MDcxNjAsImp0aSI6ImEyNTJjMmE5LWFhZTEtNDNmZi04ZjNlLTAwY2JkODU0MTUwYSJ9.hm5GhHd1FOeeUkndGFKL8r8rNcpcmS_XFDyB6a4LFUO9uLqhC08-d1qDkpesg6MKTeTBuygA4ihX6khc8PGdfZRAtfQiYQJNqGJ72nOKZ-MNKBNqn0ztVEXvu9QTeGMKSyhFSOc2sScclZtCwUCIQfFXJsa_XjzpRdE_DuYOP0w', TIMESTAMPADD(DAY, 1, CURRENT_TIMESTAMP()), 'Bearer', 1, 9, 1, NULL, NULL);
 
-INSERT INTO REFRESH_TOKEN (ID, TOKEN_VALUE, EXPIRATION, AUTH_HOLDER_ID, CLIENT_ID) VALUES
+INSERT INTO refresh_token (id, token_value, expiration, auth_holder_id, client_id) VALUES
 (1, 'eyJhbGciOiJub25lIn0.eyJqdGkiOiIwNDE4YWNkMi1hNWY3LTQ3YWItYTljZS1mZDVlZWYzNjI0MjcifQ.', NULL, 1, 9);
