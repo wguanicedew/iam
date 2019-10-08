f76fd53d49476519517a53e45afcbbe394c3855d
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2019-10-08 07:57:50 +0200

    Profile scopes for scope-aware profile resolver

--- iam-persistence/src/main/resources/db/migration/h2/V35___scope_match_policies.sql
+++ iam-persistence/src/main/resources/db/migration/h2/V35___scope_match_policies.sql
@@ -1,3 +1,8 @@
 ALTER TABLE iam_scope_policy ADD matching_policy VARCHAR(6) DEFAULT 'EQ';
 UPDATE iam_scope_policy set matching_policy = 'EQ';
 ALTER TABLE iam_scope_policy ALTER COLUMN matching_policy SET NOT NULL;
+--
+INSERT INTO system_scope(scope, description, icon, restricted, default_scope, structured, structured_param_description) 
+VALUES 
+('urn:indigo-iam.github.io:jwt-profile#wlcg-1.0','Tags a client to use the WLCG JWT profile', null, false, false, true, null),
+('urn:indigo-iam.github.io:jwt-profile#iam','Tags a client to use the IAM JWT profile', null, false, false, true, null);
