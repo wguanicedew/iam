acb8b92db9edefde5fbe90660ba204026f3df9a2
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2019-10-15 16:18:39 +0200

    Improve test coverage

--- iam-login-service/src/main/java/it/infn/mw/iam/core/oauth/scope/pdp/IamNullScopeFilter.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/core/oauth/scope/pdp/IamNullScopeFilter.java
@@ -25,10 +25,6 @@ import org.springframework.stereotype.Component;
 @ConditionalOnProperty(name="iam.enableScopeAuthz", havingValue="false", matchIfMissing=true)
 public class IamNullScopeFilter implements IamScopeFilter {
 
-  public IamNullScopeFilter() {
-    // empty constructor
-  }
-
   @Override
   public void filterScopes(Set<String> scopes, Authentication authn) {
     // do nothing
