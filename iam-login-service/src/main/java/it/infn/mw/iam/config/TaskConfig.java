cfff8f8b9426ce4087b95f5a0dde28f476475aca
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2020-04-24 14:03:47 +0200

    wip

--- iam-login-service/src/main/java/it/infn/mw/iam/config/TaskConfig.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/config/TaskConfig.java
@@ -28,7 +28,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
@@ -43,7 +42,6 @@ import it.infn.mw.iam.notification.service.NotificationStoreService;
 
 @Configuration
 @EnableScheduling
-@Profile("prod")
 public class TaskConfig implements SchedulingConfigurer {
 
   public static final Logger LOG = LoggerFactory.getLogger(TaskConfig.class);
