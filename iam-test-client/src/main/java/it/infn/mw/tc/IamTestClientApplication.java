62886b599e3980ead40bb875fbeb9a491a3a18eb
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2021-09-11 08:59:48 +0200

    Dynamic downscoping in test client app
    The test client app has been modified to allow downscoping the
    authorization request to only include a subset of the configured scopes.

--- iam-test-client/src/main/java/it/infn/mw/tc/IamTestClientApplication.java
+++ iam-test-client/src/main/java/it/infn/mw/tc/IamTestClientApplication.java
@@ -3,9 +3,6 @@ package it.infn.mw.tc;
 import java.io.IOException;
 import java.security.Principal;
 import java.text.ParseException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -14,15 +11,11 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.mitre.oauth2.model.RegisteredClient;
 import org.mitre.openid.connect.client.OIDCAuthenticationFilter;
-import org.mitre.openid.connect.client.service.AuthRequestOptionsService;
-import org.mitre.openid.connect.config.ServerConfiguration;
 import org.mitre.openid.connect.model.OIDCAuthenticationToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -53,7 +46,6 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.WebUtils;
 
-import com.google.common.base.Strings;
 import com.nimbusds.jwt.JWTParser;
 
 @SpringBootApplication
@@ -72,9 +64,6 @@ public class IamTestClientApplication extends WebSecurityConfigurerAdapter {
   @Autowired
   ClientHttpRequestFactory requestFactory;
 
-  @Value("${iam.extAuthnHint}")
-  String extAuthnHint;
-
   public static void main(String[] args) {
 
     SpringApplication.run(IamTestClientApplication.class, args);
@@ -92,37 +81,9 @@ public class IamTestClientApplication extends WebSecurityConfigurerAdapter {
 
   }
 
-  public class ExtAuthnRequestOptionsService implements AuthRequestOptionsService {
-
-    final String authnHint;
-
-    public ExtAuthnRequestOptionsService(String hint) {
-      this.authnHint = hint;
-    }
-
-    @Override
-    public Map<String, String> getOptions(ServerConfiguration server, RegisteredClient client,
-        HttpServletRequest request) {
-      Map<String, String> m = new HashMap<>();
-      m.put("ext_authn_hint", authnHint);
-      return m;
-    }
-
-    @Override
-    public Map<String, String> getTokenOptions(ServerConfiguration server, RegisteredClient client,
-        HttpServletRequest request) {
-      return Collections.emptyMap();
-    }
-
-  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-
-    if (!Strings.isNullOrEmpty(extAuthnHint)) {
-      oidcFilter.setAuthRequestOptionsService(new ExtAuthnRequestOptionsService(extAuthnHint));
-    }
-
     // @formatter:off
     http.antMatcher("/**").authorizeRequests()
         .antMatchers("/", "/user", "/error", "/openid_connect_login**", "/webjars/**").permitAll()
