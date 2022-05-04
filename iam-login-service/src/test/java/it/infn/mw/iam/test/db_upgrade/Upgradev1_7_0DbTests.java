2c9c21ec2a19749cdd2058e89f53647d04cd0199
rmiccoli <robmic.rm@gmail.com>
2022-05-04 18:06:11 +0200

    Change flyway version
    to 7.15.0. It is still supported by mysql 5.7.

--- iam-login-service/src/test/java/it/infn/mw/iam/test/db_upgrade/Upgradev1_7_0DbTests.java
+++ iam-login-service/src/test/java/it/infn/mw/iam/test/db_upgrade/Upgradev1_7_0DbTests.java
@@ -27,7 +27,7 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import it.infn.mw.iam.IamLoginService;
-import it.infn.mw.iam.test.util.db.MySQL80TestContainer;
+import it.infn.mw.iam.test.util.db.MySQL57TestContainer;
 
 @Testcontainers(disabledWithoutDocker = true)
 public class Upgradev1_7_0DbTests extends UpgradeDbTestSupport {
@@ -35,8 +35,8 @@ public class Upgradev1_7_0DbTests extends UpgradeDbTestSupport {
   public static final String DB_DUMP = "iam-v1.7.0-mysql5.7.sql";
 
   @Container
-  static MySQL80TestContainer db =
-      new MySQL80TestContainer().withClasspathResourceMapping(
+  static MySQL57TestContainer db =
+      new MySQL57TestContainer().withClasspathResourceMapping(
           joinPathStrings(DB_DUMPS_DIR, DB_DUMP), joinPathStrings(INITDB_DIR, DB_DUMP),
           BindMode.READ_ONLY);
 
--- iam-login-service/src/test/java/it/infn/mw/iam/test/util/db/MySQL57TestContainer.java
+++ iam-login-service/src/test/java/it/infn/mw/iam/test/util/db/MySQL57TestContainer.java
@@ -17,14 +17,14 @@ package it.infn.mw.iam.test.util.db;
 
 import org.testcontainers.containers.MySQLContainer;
 
-public class MySQL80TestContainer extends MySQLContainer<MySQL80TestContainer> {
+public class MySQL57TestContainer extends MySQLContainer<MySQL57TestContainer> {
 
-  public static final String DEFAULT_IMAGE = "mysql:8.0";
+  public static final String DEFAULT_IMAGE = "mysql:5.7";
   public static final String DEFAULT_DATABASE_NAME = "iam";
   public static final String DEFAULT_USERNAME = "iam";
   public static final String DEFAULT_PASSWORD = "pwd";
 
-  public MySQL80TestContainer() {
+  public MySQL57TestContainer() {
     super(DEFAULT_IMAGE);
     withDatabaseName(DEFAULT_DATABASE_NAME);
     withPassword(DEFAULT_PASSWORD);
