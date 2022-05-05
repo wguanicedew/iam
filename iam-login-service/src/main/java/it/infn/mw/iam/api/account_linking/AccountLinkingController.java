502c61d59f9c09d0ecc95f22be3a98ac738b3239
rmiccoli <robmic.rm@gmail.com>
2022-05-05 17:52:28 +0200

    Add a condition for principal null

--- iam-login-service/src/main/java/it/infn/mw/iam/api/account_linking/AccountLinkingController.java
+++ iam-login-service/src/main/java/it/infn/mw/iam/api/account_linking/AccountLinkingController.java
@@ -129,7 +129,10 @@ public class AccountLinkingController extends ExternalAuthenticationHandlerSuppo
 
     checkAccountLinkingEnabled(redirectAttributes);
     HttpSession session = request.getSession();
-    principal = getAccountLinkingSavedAuthentication(session);
+
+    if (principal == null) {
+      principal = getAccountLinkingSavedAuthentication(session);
+    }
 
     if (!hasAccountLinkingDoneKey(session)) {
       throw new IllegalArgumentException("No account linking done key found in request.");
